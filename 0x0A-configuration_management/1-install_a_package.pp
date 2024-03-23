4bb030084aaa2b92c753eb1292c850bd99385c4a
borisngong <borisngong@gmail>
2024-03-22 17:14:20 -0700

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-file { '/tmp/school':
-  ensure  => 'present',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# Using Puppet, A manifest to install flask version 2.1.0 from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
+
