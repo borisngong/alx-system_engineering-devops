c3189bf68df73ba558ce404976c4e26973433b6f
borisngong <borisngong@gmail>
2024-03-23 19:54:19 -0700

    client configuration with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # Using Puppet to write a manifest for client configuration file
 
-file { 'etc/ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
 	ensure  => 'present',
 }
 
 file_line { 'disable password authentication':
-	path  => 'etc/ssh/ssh_config',
+	path  => '/etc/ssh/ssh_config',
 	line  => 'PasswordAuthentication no',
 	match => '#PasswordAuthentication',
 }
 
 file_line { 'Add Identity file':
-	path  => 'etc/ssh/ssh_config'
-	line  => 'IdentityFile ~/.ssh/school'
+	path  => '/etc/ssh/ssh_config',
+	line  => 'IdentityFile ~/.ssh/school',
 	match => '#IdentityFile',
 }
