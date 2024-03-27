9bb1e41008d950552533a4d33713d4ae0d767c68
borisngong <borisngong@gmail>
2024-03-26 19:51:55 -0700

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,58 @@
+# Puppet Manifest to install and configure Nginx web server with 301 redirect
+
+# Install Nginx package
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Ensure Nginx service is enabled and running
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+  require => Package['nginx'],
+}
+
+# Configure Nginx server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html;
+
+        location / {
+            try_files $uri $uri/ =404;
+        }
+
+        location /redirect_me {
+            return 301 https://github.com/borisngong;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+            root /usr/share/nginx/html;
+            internal;
+        }
+    }
+  ",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Create index.html file with "Hello World!"
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => "Hello World!\n",
+}
+
+# Test Nginx configuration
+exec { 'nginx-config-test':
+  command     => '/usr/sbin/nginx -t',
+  path        => ['/usr/sbin', '/usr/bin', '/bin'],
+  refreshonly => true,
+  notify      => Service['nginx'],
+}
+
