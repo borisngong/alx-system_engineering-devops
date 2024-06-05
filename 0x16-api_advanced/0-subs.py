47d2f46461ff7afd4bbe744b9653c53ced23893e
borisngong <borisngong@gmail>
2024-06-04 19:40:41 -0700

    Queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,6 +18,7 @@ def number_of_subscribers(subreddit):
     j_response = requests.get(url, headers=headers, allow_redirects=False)
 
     if j_response.status_code == 200:
-        return j_response.json().get("data").get("subscribers")
+        json_data = j_response.json()
+        return json_data['data']['subscribers']
     else:
         return 0
