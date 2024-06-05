4d3e55eed12c2aefaccb29644550c06938375942
borisngong <borisngong@gmail>
2024-06-04 19:27:00 -0700

    Queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     In case subreddit is invalid, return 0
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyAPI/0.1'}
+    headers = {'User-Agent': 'My-User-Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
