998d5406b3c88a6c1cb6aa3d6db968cce67c8c7f
borisngong <borisngong@gmail>
2024-08-13 16:48:48 -0700

    A recursive function that queries the Reddit API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-"""Module Responsible for Querying Reddit API Recursively"""
+
+
+"""Module responsible for querrying reddit API recursively"""
 
 
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Responsible for returning all hot posts on a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    users = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by boro)"
-    }
-    parameters = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=users, params=parameters,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], after=''):
+    """
+    A recursive function that queries the Reddit API and
+    returns a list containing the titles of all hot articles
+    for a given subreddit. If no results are found for
+    the given subreddit, the function returns None.
+    """
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'user-agent': 'redquery'}
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers,
+                            params=parameters, allow_redirects=False)
+
+    if response.status_code != 200:
         return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
+    hot = response.json()['data']['children']
+    aft = response.json()['data']['after']
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    for post in hot:
+        hot_list.append(post['data']['title'])
+
+    if aft:
+        recurse(subreddit, hot_list, aft)
+    else:
+        return hot_list
